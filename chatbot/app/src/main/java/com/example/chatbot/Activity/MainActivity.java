0ff04b6e215bdc7c8a30d7ae8b93076080ecf923
therider736 <nguyentuancuong736@gmail.com>
2020-07-29 20:41:10 +0700

    check internet connection, handle speech to text & record message

--- chatbot/app/src/main/java/com/example/chatbot/Activity/MainActivity.java
+++ chatbot/app/src/main/java/com/example/chatbot/Activity/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.chatbot.Activity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chatbot.Adapter.ChatAdapter;
+import com.example.chatbot.Listener.ClickListener;
+import com.example.chatbot.Listener.RecyclerTouchListener;
+import com.example.chatbot.Model.Message;
 import com.example.chatbot.R;
+import com.ibm.cloud.sdk.core.http.HttpMediaType;
 import com.ibm.cloud.sdk.core.http.Response;
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
 import com.ibm.watson.assistant.v2.Assistant;
@@ -17,15 +32,21 @@ import com.ibm.watson.assistant.v2.model.SessionResponse;
 import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneHelper;
 import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneInputStream;
 import com.ibm.watson.developer_cloud.android.library.audio.StreamPlayer;
+import com.ibm.watson.developer_cloud.android.library.audio.utils.ContentType;
 import com.ibm.watson.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionResults;
+import com.ibm.watson.speech_to_text.v1.websocket.BaseRecognizeCallback;
 import com.ibm.watson.text_to_speech.v1.TextToSpeech;
+import com.ibm.watson.text_to_speech.v1.model.SynthesizeOptions;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ChatAdapter mAdapter;
-    private ArrayList messageArrayList;
+    private ArrayList<Message> messageArrayList = new ArrayList<>();
     private EditText inputMessage;
     private ImageButton btnSend;
     private ImageButton btnRecord;
@@ -64,5 +85,175 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mContext = getApplicationContext();
+
+        inputMessage = findViewById(R.id.message);
+        btnSend = findViewById(R.id.btn_send);
+        btnRecord = findViewById(R.id.btn_record);
+        recyclerView = findViewById(R.id.recycler_view);
+
+        mAdapter = new ChatAdapter(messageArrayList);
+        microphoneHelper = new MicrophoneHelper(this);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+        this.inputMessage.setText("");
+        this.initialRequest = true;
+
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to record denied!");
+        } else Log.i(TAG, "Permission to record was already granted!");
+
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(mContext, recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Message audioMessage = messageArrayList.get(position);
+                if (audioMessage != null && !audioMessage.getMessage().isEmpty()) {
+                    new SayTask().execute(audioMessage.getMessage());
+                }
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                recordMessage();
+            }
+        }));
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkInternetConnection()) {
+                    sendMessage();
+                }
+            }
+        });
+    }
+
+    //Check Internet Connection
+    private boolean checkInternetConnection() {
+        //Get Connectivity Manager object to check connection
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        //Check for network connections
+        if (isConnected) {
+            return true;
+        } else {
+            Toast.makeText(this, "No Internet Connection available!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    //Handling voice of Bot
+    private class SayTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            streamPlayer.playStream(textToSpeech.synthesize(new SynthesizeOptions.Builder()
+                    .text(params[0])
+                    .voice(SynthesizeOptions.Voice.EN_US_LISAVOICE)
+                    .accept(HttpMediaType.AUDIO_WAV)
+                    .build()).execute().getResult());
+
+            return "Did synthesize";
+        }
+    }
+
+    //Watson Speech to Text Methods
+    private class MicrophoneRecognizeDelegate extends BaseRecognizeCallback {
+        @Override
+        public void onTranscription(SpeechRecognitionResults speechResults) {
+            if (speechResults.getResults() != null && !speechResults.getResults().isEmpty()) {
+                String text = speechResults.getResults().get(0).getAlternatives().get(0).getTranscript();
+                showMicCheck(text);
+            }
+        }
+
+        @Override
+        public void onError(Exception e) {
+            showError(e);
+            enableMicButton();
+        }
+
+        @Override
+        public void onDisconnected() {
+            enableMicButton();
+        }
+    }
+
+    //Private methods - Speech to Text
+    private RecognizeOptions getRecognizeOptions(InputStream audio) {
+        return new RecognizeOptions.Builder()
+                .audio(audio)
+                .contentType(ContentType.OPUS.toString())
+                .model("en-US_BroadbandModel")
+                .interimResults(true)
+                .inactivityTimeout(2000)
+                .build();
+    }
+
+    //Record a message via Speech to Text
+    private void recordMessage() {
+        if (!listening) {
+            capture = microphoneHelper.getInputStream(true);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        speechToText.recognizeUsingWebSocket(getRecognizeOptions(capture), new MicrophoneRecognizeDelegate());
+                    } catch (Exception e) {
+                        showError(e);
+                    }
+                }
+            }).start();
+            listening = true;
+            Toast.makeText(MainActivity.this, "Listening...Click to Stop!", Toast.LENGTH_SHORT).show();
+        } else {
+            try {
+                microphoneHelper.closeInputStream();
+                listening = false;
+                Toast.makeText(MainActivity.this, "Stopped Listening...Click to Start", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //Send a message to Watson Assistant Service
+    private void sendMessage() {
+        
+    }
+
+    private void showMicCheck(final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                inputMessage.setText(text);
+            }
+        });
+    }
+
+    private void enableMicButton() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                btnRecord.setEnabled(true);
+            }
+        });
+    }
+
+    private void showError(final Exception e) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        });
     }
 }
